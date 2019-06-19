c97db8c22ab9acbf2a24876c6137ceca33684f2d
ryanbhouser <ryanbhouser>
2019-06-19 17:32:35 -0400

    Add books to database

--- routes/books.js
+++ routes/books.js
@@ -12,4 +12,29 @@ router.get('/', (req, res, next) => {
   });
 });
 
+// Create a new book
+router.get('/new', (req, res, next) => {
+  res.render('books/new', {
+    books: Books.build(),
+    title: 'Add a New Book'
+  });
+});
+
+// POST create book
+router.post('/', (req, res, next) => {
+  Books.create(req.body).then(book => {
+    res.redirect('/books/' + book.id);
+  });
+});
+
+// GET individual book
+router.get('/:id', (req, res, next) => {
+  Books.findByPk(req.params.id).then(book => {
+    res.render('books/show', {
+      books: book,
+      title: book.title
+    });
+  });
+});
+
 module.exports = router;
